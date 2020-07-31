9ec96e440b1b78f578081b13f65f0b0a4d838047
jhmoon <jhmoon@hancom.com>
2020-07-31 14:11:30 +0900

    add1

--- test.js
+++ test.js
@@ -4,4 +4,12 @@
 	"type": "text",
 	"sheet": 0,
 	"o": [2, 2, 2, 2]
+},
+{
+	"cmd": "cInsert", "value": "3", "type": "text", "sheet": 0, "o": [3, 2, 3, 2]}, {
+	"cmd": "cInsert",
+	"value": "=SUM(C3,C4)",
+	"type": "text",
+	"sheet": 0,
+	"o": [2, 3, 2, 3]
 }
